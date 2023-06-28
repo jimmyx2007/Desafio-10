7839f38d687ad8864da77a14ad308f48a8331aa8
jimmyx2007 <jimmyx2486@gmail.com>
2023-06-28 01:21:41 -0300

    Agregue estructura web

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, contacto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('contacto/', contacto, name='contacto')
 ]
